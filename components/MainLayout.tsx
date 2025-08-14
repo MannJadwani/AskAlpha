ea118480d7a0f495ffd6e3ea2bc41cc0f7c23e41
Mann Jadwani <mannjadwani2004@gmail.com>
2025-08-14 17:09:49 +0530

    analytics vercel

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -3,23 +3,28 @@
 
 import { ReportProvider } from "@/context/ReportContext";
 import { AuthProvider } from "@/context/AuthContext";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   
   
   return (
-    <AuthProvider>
-      <ReportProvider>
-        <div className="min-h-screen" style={{ background: 'var(--background)', color: 'var(--foreground)' }}>
-       
-          
-          <div >
-            <main >
-              {children}
-            </main>
-          </div>
-        </div>
-      </ReportProvider>
-    </AuthProvider>
+<div>
+<Analytics />
+
+<AuthProvider>
+  <ReportProvider>
+    <div className="min-h-screen" style={{ background: 'var(--background)', color: 'var(--foreground)' }}>
+   
+      
+      <div >
+        <main >
+          {children}
+        </main>
+      </div>
+    </div>
+  </ReportProvider>
+</AuthProvider>
+</div>
   );
 } 
